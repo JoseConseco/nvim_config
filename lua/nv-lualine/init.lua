b8248f596390caf674a260373c96193962b9e112
JoseConseco <bartosz.styperek@gmail.com>
2025-09-20 12:03:18 +0200

    depreciation fix

--- lua/nv-lualine/init.lua
+++ lua/nv-lualine/init.lua
@@ -100,7 +100,7 @@ local lsp = {
   -- Lsp server name .
   function()
     local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     local out_clients = {}
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
